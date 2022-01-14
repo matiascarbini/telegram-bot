270bbcf74e4a5a37220534f5c8877c0ab02902d9
Matias Carbini <matias.carbini@gmail.com>
2022-01-14 19:48:06 -0300

    bug

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 TOKEN = os.environ.get('TOKEN')
-PUERTO = int(os.environ.get('PORT', 5000))
+PORT = int(os.environ.get('PORT', 5000))
 
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
